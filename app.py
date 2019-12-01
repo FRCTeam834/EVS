fcb724a6178d2f8c0dcfc2d2dc61c5c7a2898be9
Christian Piper <xmanpiper@gmail.com>
2019-12-01 18:28:22 -0500

    Did more work, works with test model independently

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ import edgeiq
 import pyfrc
 from networktables import NetworkTables
 import logging
+import numpy as np
 
 # Constant for the default confidence (0 being 0% sure and 1 being 100% sure)
-default_conf_thres = .5
+default_conf_thres = .75
 
 # TODO: Order the predictions in terms of priority (proximity?)
 
 def main():
+    # Allow Rio to boot and configure network
+    time.sleep(5.0)
 
     # Setup logging for the NetworkTables messages
     logging.basicConfig(level=logging.DEBUG)
@@ -18,39 +21,39 @@ def main():
     NetworkTables.initialize(server = '10.8.34.2')
 
     # Create table for values
-    val = NetworkTables.getTable('VisionValues')
-    sd = NetworkTables.getTable('SmartDashboard')
+    evs = NetworkTables.getTable('EVS')
+    # sd = NetworkTables.getTable('SmartDashboard')
 
     # Create sub-tables and append them to arrays: 3 for hatches, 3 for balls, and 6 for tape
     hatchTables = []
     ballTables = []
     tapeTables = []
 
-    hatch0 = val.getSubTable('Hatch0')
+    hatch0 = evs.getSubTable('Hatch0')
     hatchTables.append(hatch0)
-    hatch1 = val.getSubTable('Hatch1')
+    hatch1 = evs.getSubTable('Hatch1')
     hatchTables.append(hatch1)
-    hatch2 = val.getSubTable('Hatch2')
+    hatch2 = evs.getSubTable('Hatch2')
     hatchTables.append(hatch2)
 
-    ball0 = val.getSubTable('Ball0')
+    ball0 = evs.getSubTable('Ball0')
     ballTables.append(ball0)
-    ball1 = val.getSubTable('Ball1')
+    ball1 = evs.getSubTable('Ball1')
     ballTables.append(ball1)
-    ball2 = val.getSubTable('Ball2')
+    ball2 = evs.getSubTable('Ball2')
     ballTables.append(ball2)
     
-    tape0 = val.getSubTable('Tape0')
+    tape0 = evs.getSubTable('Tape0')
     tapeTables.append(tape0)
-    tape1 = val.getSubTable('Tape1')
+    tape1 = evs.getSubTable('Tape1')
     tapeTables.append(tape1)
-    tape2 = val.getSubTable('Tape2')
+    tape2 = evs.getSubTable('Tape2')
     tapeTables.append(tape2)
-    tape3 = val.getSubTable('Tape3')
+    tape3 = evs.getSubTable('Tape3')
     tapeTables.append(tape3)
-    tape4 = val.getSubTable('Tape4')
+    tape4 = evs.getSubTable('Tape4')
     tapeTables.append(tape4)
-    tape5 = val.getSubTable('Tape5')
+    tape5 = evs.getSubTable('Tape5')
     tapeTables.append(tape5)
 
         # Setup EdgeIQ
@@ -67,11 +70,10 @@ def main():
     # Get the FPS
     fps = edgeiq.FPS()
 
-    sd.putString('DB/String 3', default_conf_thres)
+    # sd.putString('DB/String 3', default_conf_thres)
 
     try:
-        with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
-                edgeiq.Streamer() as streamer:
+        with edgeiq.WebcamVideoStream(cam=0) as video_stream:
             # Allow Webcam to warm up
             time.sleep(2.0)
             fps.start()
@@ -84,63 +86,53 @@ def main():
 
             # loop detection
             while True:
-
-                # Grab value for confidence from SmartDashboard, if it can't, use default
-                confidence_thres = val.getString('DB/String 3', default_conf_thres)
-
-                try:
-                    # Try converting string to a float
-                    confidence_thres = float(confidence_thres)
-                except:
-                    # If that fails, set the confidence threshold to the default value
-                    confidence_thres = default_conf_thres
+                confidence_thres = default_conf_thres
 
                 frame = video_stream.read()
                 results = obj_detect.detect_objects(frame, confidence_level = confidence_thres)
-                frame = edgeiq.markup_image(
-                        frame, results.predictions, colors=obj_detect.colors)
+                #frame = edgeiq.markup_image(
+                #        frame, results.predictions, colors=obj_detect.colors)
 
                 #Counters - they reset after every frame in the while loop
                 hatchCounter = 0
                 ballCounter = 0
                 tapeCounter = 0
                                         
-                # Update the VisionValues NetworkTable with new values
+                # Update the EVS NetworkTable with new values
                 for prediction in results.predictions:                                                                                                                        
 
+                    center_x, center_y = prediction.box.center
                     # Code goes here
-                    numValues = [prediction.box.center, prediction.box.end_x, prediction.box.end_y, prediction.box.area, (prediction.confidence*100)]
+                    numValues = [center_x, center_y, prediction.box.end_x, prediction.box.end_y, prediction.box.area, (prediction.confidence * 100)]
+                    
+                    for entry in range(0, len(numValues) - 1):
+                        numValues[entry] = round(numValues[entry], 3)
+
+                    numValuesArray = np.asarray(numValues)
                     
                     #
                     # IMPORTANT:
                     # Names of labels have not been decided yet
                     #
                     #
-                    if prediction.label == "Hatch":
-                        # Do label separately because it is a string
-                        hatchTables[hatchCounter].putString('label', prediction.label)
-
-                        hatchTables[hatchCounter].putNumberArray('values', numValues)
+                    if prediction.label == "train": # Hatches = trains
+                    
+                        hatchTables[hatchCounter].putNumberArray('values', numValuesArray)
                         # Boolean asks to update
                         hatchTables[hatchCounter].putBoolean('inUse', True)
 
                         hatchCounter += 1
 
-                    if prediction.label == "Ball":
-                        # Do label separately because it is a string
-                        ballTables[ballCounter].putString('label', prediction.label)
+                    elif prediction.label == "car": # Balls = cars 
 
-                        ballTables[ballCounter].putNumberArray('values', numValues)
+                        ballTables[ballCounter].putNumberArray('values', numValuesArray)
                         # Boolean asks to update
                         ballTables[ballCounter].putBoolean('inUse', True)
-
                         ballCounter += 1
 
-                    if prediction.label == "Tape":
-                        # Do label separately because it is a string
-                        tapeTables[tapeCounter].putString('label', prediction.label)
+                    elif prediction.label == "person": # Tape = people
 
-                        tapeTables[tapeCounter].putNumberArray('values', numValues)
+                        tapeTables[tapeCounter].putNumberArray('values', numValuesArray)
                         # Boolean asks to update
                         tapeTables[tapeCounter].putBoolean('inUse', True)
 
@@ -151,6 +143,8 @@ def main():
                 ballTables[ballCounter].putBoolean('inUse', False)
                 tapeTables[tapeCounter].putBoolean('inUse', False)
 
+                evs.putBoolean('checked', False)
+                '''
                 # Generate text to display on streamer
                 text = ["Model: {}".format(obj_detect.model_id)]
                 text.append("Inference time: {:1.3f} s".format(results.duration))
@@ -162,12 +156,12 @@ def main():
                         prediction.label, prediction.confidence * 100))
 
                 streamer.send_data(frame, text)
-
+                '''
                 fps.update()
-
+                '''
                 if streamer.check_exit():
                     break
-
+                '''
     finally:
         fps.stop()
         print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
